66996e78b4a01ba5fedee161b28e392f756e569a
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2023-09-23 15:44:34 +0530

    Updated

--- client/src/components/home/Navbarhome.js
+++ client/src/components/home/Navbarhome.js
@@ -1,30 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Navbarhome = () => {
-    return (
-        <div>
-            <nav>
-                <div class="nav-wrapper green darken-10">
-                    <a href="#!" class="brand-logo right">AsHuToSh ChAtRoOm</a>
-                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                    <ul class="left hide-on-med-and-down">
-                        <li><a href="/login">Login</a></li>
-                        <li><a href="/signup">Sign-up</a></li>
-                        
-                    </ul>
-                </div>
-            </nav>
-
-            <ul class="sidenav" id="mobile-demo">
-                <li><a href="/login">Login</a></li>
-    
-                <li><a href="/signup">Sign-up</a></li>
-                
-            </ul>
+  return (
+    <div>
+      <nav>
+        <div class="nav-wrapper green darken-10">
+          <a href="#!" class="brand-logo right">
+            Pawan Talk Hub
+          </a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
+          <ul class="left hide-on-med-and-down">
+            <li>
+              <a href="/login">Login</a>
+            </li>
+            <li>
+              <a href="/signup">Sign-up</a>
+            </li>
+          </ul>
         </div>
+      </nav>
 
+      <ul class="sidenav" id="mobile-demo">
+        <li>
+          <a href="/login">Login</a>
+        </li>
 
-    )
-}
+        <li>
+          <a href="/signup">Sign-up</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbarhome
+export default Navbarhome;
--- client/src/components/home/Navbaruser.js
+++ client/src/components/home/Navbaruser.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Navbaruser = () => {
-    const logout = async () => {
-        localStorage.removeItem('loginData')
-    }
-    return (
-        <div>
-            <nav>
-                <div class="nav-wrapper green darken-10">
-                    <a href="#!" class="brand-logo right">AsHuToSh's ChAtRoOm</a>
-                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                    <ul class="left hide-on-med-and-down">
-                        <li onClick={logout}><a href="/">Logout</a></li>
-                        <li><a href="/signup">Sign-up</a></li>
-
-                    </ul>
-                </div>
-            </nav>
-
-            <ul class="sidenav" id="mobile-demo">
-                <li onClick={logout}><a href="/">Logout</a></li>
-                <li><a href="/signup">Sign-up</a></li>
-
-            </ul>
+  const logout = async () => {
+    localStorage.removeItem("loginData");
+  };
+  return (
+    <div>
+      <nav>
+        <div class="nav-wrapper green darken-10">
+          <a href="#!" class="brand-logo right">
+            Pawan Talk Hub
+          </a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
+          <ul class="left hide-on-med-and-down">
+            <li onClick={logout}>
+              <a href="/">Logout</a>
+            </li>
+            <li>
+              <a href="/signup">Sign-up</a>
+            </li>
+          </ul>
         </div>
-         
-    )
-}
+      </nav>
 
-export default Navbaruser
+      <ul class="sidenav" id="mobile-demo">
+        <li onClick={logout}>
+          <a href="/">Logout</a>
+        </li>
+        <li>
+          <a href="/signup">Sign-up</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbaruser;
